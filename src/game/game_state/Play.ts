ca216c418f8eff9d9d46b7d3852441592b8ec771
pierallard <pierallard@gmail.com>
2019-04-14 19:36:32 +0200

    Add tween example

--- src/game/game_state/Play.ts
+++ src/game/game_state/Play.ts
@@ -1,25 +1,29 @@
 export default class Play extends Phaser.State {
-    private bitMapText;
-
     constructor() {
         super();
     }
 
     public create(game: Phaser.Game) {
-      this.bitMapText = game.add.bitmapText(100,100, 'font', 'Sample text', 7);
+      /* Text example */
+      game.add.bitmapText(100,100, 'font', 'Sample text', 7);
+
+      /* Graphics example */
       const graphics = game.add.graphics(100, 150);
       graphics.lineStyle(2, 0xffff00);
       graphics.drawRect(0, 0, 50, 50);
+
+      /* Image example */
       game.add.image(100, 250, 'chips', 0);
+
+      /* Animated sprite example */
       const sprite = game.add.sprite(100, 300, 'chips', 1);
       sprite.animations.add('animationName', [1, 2, 3, 4], 4, true);
       sprite.animations.play('animationName');
+
+      /* Tween example */
+      game.add.tween(sprite).to( { x: 500 }, 4000, Phaser.Easing.Default, true);
     }
 
     public update(game: Phaser.Game) {
-        this.bitMapText.x += 0.5;
-        if (this.bitMapText.x > 500) {
-            this.bitMapText.x = 100;
-        }
     }
 }
