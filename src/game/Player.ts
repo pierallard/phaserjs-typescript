966fd3e2e4c306788ea464f66828a2bb15fc2ac1
pierallard <pierallard@gmail.com>
2019-04-15 00:12:57 +0200

    fix anims

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -39,10 +39,10 @@ export default class Player {
     this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-    this.sprite.animations.add(Player.ANIMATION_LEFT, [101, 102, 103, 104, 105], Phaser.Timer.SECOND * 5 / TIME, false);
-    this.sprite.animations.add(Player.ANIMATION_RIGHT, [133, 134, 135, 136, 137], Phaser.Timer.SECOND * 5 / TIME, true);
-    this.sprite.animations.add(Player.ANIMATION_UP, [96, 97, 98, 99, 100], Phaser.Timer.SECOND * 5 / TIME, false);
-    this.sprite.animations.add(Player.ANIMATION_DOWN, [128, 129, 130, 131, 132], Phaser.Timer.SECOND * 5 / TIME, false);
+    this.sprite.animations.add(Player.ANIMATION_LEFT, [101, 102, 103, 104, 105, 104, 103, 102], Phaser.Timer.SECOND * 4 / TIME, true);
+    this.sprite.animations.add(Player.ANIMATION_RIGHT, [133, 134, 135, 136, 137, 136, 135, 134], Phaser.Timer.SECOND * 4 / TIME, true);
+    this.sprite.animations.add(Player.ANIMATION_UP, [96, 97, 98, 99, 100, 99, 98, 97], Phaser.Timer.SECOND * 4 / TIME, true);
+    this.sprite.animations.add(Player.ANIMATION_DOWN, [128, 129, 130, 131, 132, 131, 130, 129], Phaser.Timer.SECOND * 4 / TIME, true);
 
     game.camera.follow(this.sprite);
   }
@@ -92,7 +92,9 @@ export default class Player {
 
   private runAnimation(game: Phaser.Game, animationName: string, gapX: number, gapY: number) {
     this.isProcessing = true;
-    this.sprite.animations.play(animationName);
+    if (this.sprite.animations.currentAnim !== this.sprite.animations.getAnimation(animationName)) {
+      this.sprite.animations.play(animationName);
+    }
     const newPosition = new PIXI.Point(this.position.x + gapX, this.position.y + gapY);
     game.add.tween(this.sprite).to({
       x: Player.getPosition(newPosition).x,
@@ -106,21 +108,25 @@ export default class Player {
       this.position.y += gapY;
       this.sprite.x = Player.getPosition(this.position).x;
       this.sprite.y = Player.getPosition(this.position).y;
-
-      this.sprite.animations.stop();
-      if (animationName === Player.ANIMATION_LEFT) {
-        this.sprite.frame = 103;
-      } else if (animationName === Player.ANIMATION_RIGHT) {
-        this.sprite.frame = 135;
-      } else if (animationName === Player.ANIMATION_UP) {
-        this.sprite.frame = 98;
-      } else if (animationName === Player.ANIMATION_DOWN) {
-        this.sprite.frame = 130;
+      if (!this.pressedKeys.length) {
+        this.sprite.animations.stop();
+        this.sprite.animations.currentAnim = null;
+        if (animationName === Player.ANIMATION_LEFT) {
+          this.sprite.frame = 103;
+        } else if (animationName === Player.ANIMATION_RIGHT) {
+          this.sprite.frame = 135;
+        } else if (animationName === Player.ANIMATION_UP) {
+          this.sprite.frame = 98;
+        } else if (animationName === Player.ANIMATION_DOWN) {
+          this.sprite.frame = 130;
+        }
       }
     }, this)
   }
 
   private runBlocked(game: Phaser.Game, animationName: string, key: Phaser.Key) {
+    this.sprite.animations.stop();
+    this.sprite.animations.currentAnim = null;
     if (animationName === Player.ANIMATION_LEFT) {
       this.sprite.frame = 113;
     } else if (animationName === Player.ANIMATION_RIGHT) {
